c92debdde03a8d5a8b44f477b97d403ad932d6a8
Oduntan Fawaz <oduntanfawaz@gmail.com>
2023-09-06 00:52:13 +0100

    Update

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-def islower(c):
-    if ord(c) in range(41, 91):
-        return True
-    else:
-        return False
+def uppercase(str):
+    for i in str:
+        if ord(i) in range(97, 123):
+            print(chr(ord(i) - 32))
+        else:
+            print(i)
