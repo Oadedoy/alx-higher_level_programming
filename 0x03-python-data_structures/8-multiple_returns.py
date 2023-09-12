58a78427a017dbda50e2f52d32a6e864c046cd4e
Oduntan Fawaz <oduntanfawaz@gmail.com>
2023-09-12 19:48:33 +0100

    Update

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if len(sentence) == 0:
-        s_len = None
-        first_char = None
-
-    else:
-        s_len = len(sentence)
-        first_char = sentence[0]
-    n_tuple = (s_len, first_char)
-    return n_tuple
+    s_len = len(sentence)
+    
+    if s_len == 0:
+        return (s_len, None)
+    return (s_len, sentence[0])
