58b1fcaecb89172f5dabfd3c44e2d720e725941b
Oduntan Fawaz <oduntanfawaz@gmail.com>
2023-09-12 19:41:43 +0100

    Update

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if len(sentence) == 0:
+        s_len = None
+
+    else:
+        s_len = len(sentence)
+        first_char = sentence[0]
+    n_tuple = (s_len, first_char)
+    return n_tuple
