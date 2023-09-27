aa616ea3de4436ca7b6749b5a984d8463cc6d440
Oduntan Fawaz <oduntanfawaz@gmail.com>
2023-09-27 20:32:42 +0100

    Update

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-
 def safe_print_division(a, b):
     try:
-        division = a / b
+        result = a / b
     except Exception:
-        division = None
+        result = None
     finally:
-        print("Inside division: {}".format(division))
-        return (division)
+        print("Inside result: {}".format(result))
+        return (result)
