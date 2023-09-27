ee05e485f9157c455055b30e770b12b76df3ba84
Oduntan Fawaz <oduntanfawaz@gmail.com>
2023-09-27 21:23:01 +0100

    Update

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+class Square:
+    def __init__(self, size):
+        """initialize square
+        Args:
+            size (int): size of the square
+        """
+        self.__size = size
