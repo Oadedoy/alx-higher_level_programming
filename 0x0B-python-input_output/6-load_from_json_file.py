9e05621348feeb6c3436bdb2bc84f679bfc70056
Oduntan Fawaz <oduntanfawaz@gmail.com>
2023-10-10 16:50:07 +0100

    Update

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-""" Module that creates an object from a JSON file
+""" Module that creates an Object from a JSON file
 """
 import json
 
 def load_from_json_file(filename):
-    """ Function that creates an object from a JSON file
+    """ Function that creates an Object from a JSON file
 
     Args:
         filename: name of textfile
